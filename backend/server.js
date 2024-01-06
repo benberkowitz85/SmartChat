5bcd348860bd0bcd5ed1178ad74882a0d32cbf05
lyubomirg1995 <lyubomirg17@gmail.com>
2024-01-06 16:42:02 -0500

    add: routes, controllers, mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const { chats } = require('./data/data');
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = ("./routes/userRoutes");
 
 
 const app = express();
-dotenv.config()
+dotenv.config();
+connectDB();
+
 
 app.get("/", (req,res) => {
     res.send("API is running");
 })
 
-app.get("/api/chat", (req, res) => {
-    res.send(chats);
-})
-
-app.get("/api/chat/:id", (req, res) => {
-//    console.log(req.params.id);
-const singleChat = chats.find((c)=>c._id === req.params.id)
-res.send(singleChat);
-});
+app.use("/api/user", userRoutes)
 
 const PORT = process.env.PORT || 5000
 
