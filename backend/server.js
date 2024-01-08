ddc5830074d656b03b64931215ece81ff4fb988b
lyubomirg1995 <lyubomirg17@gmail.com>
2024-01-08 14:07:52 -0500

    fix: mongdodb server loading

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { chats } = require('./data/data');
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const userRoutes = ("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 
 const app = express();
