37b372ceb731cc571c8818d1e9fa13efc4cedf96
lyubomirg1995 <lyubomirg17@gmail.com>
2024-01-09 13:14:30 -0500

    debug effort: fix user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -44,7 +44,7 @@ const authUser = asyncHandler(async(req,res) => {
     const {email, password} = req.body;
     const user = await User.find({email})
 
-    if (user && (await User.matchPassword(password))) {
+    if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
