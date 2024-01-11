9ca2515b7ea166bc21580f95cf855f0a62dc6ca4
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2024-01-11 18:10:29 -0500

    Reformulation code in file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,39 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider
-} from 'react-router-dom'
+import {Heading, Text, Box, Flex } from "@chakra-ui/react";
+import './App.css'
 
-//layouts and pages
-import RootLayout from './layout/RootLayout'
-import Dashboard from './pages/Dashboard'
-import Create from './pages/Create'
-import Profile from './pages/Profile'
-
-// router and routes
-const router = createBrowserRouter(
-         createRoutesFromElements(
-            <Route path="/" element={<RootLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="create" element={<Create />} />
-            <Route path="profile" element={<Profile />} />
-        </Route>
-    )
+function App() {
+  return (
+    <Flex
+        width={"100vw"}
+        heigth={"100vh"}
+        alignContent={"center"}
+        justifyContent={"center"}
+    >
+      <Box maxW="2xl" m=") auto">
+        <Heading as="h1" textAlign="center" fontSize="5xl" mt="100px">
+             This is SmartChat!
+        </Heading>
+        <Text fontSize="xl" textAlign="center" mt="30px">
+          This is a page made it with Chakra, Vite and React!
+        </Text>
+        <Text
+          w="fit-content"
+          p="4"
+          px="50"
+          bg="blue.300"
+          borderRadius="10px"
+          m="0 auto"
+          mt="8"
+          fontWeight="bold"
+          color="white"
+          fontSize="xl"
+      >
+        Get Started
+      </Text>
+      </Box>
+    </Flex>   
+      
   )
+}
 
-  function App() {
-    return (
-      <RouterProvider router={router} />
-
-    )
-  }
-
-  export default App
+export default App
