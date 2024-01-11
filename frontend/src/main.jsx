41649cdbf608eb8aa2aeb506a220b63be4848f6a
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2024-01-11 17:54:11 -0500

    file reformulation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
 import { ChakraProvider } from '@chakra-ui/react'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
- <React.StrictMode>
-     <ChakraProvider>
-    <App />
-    </ChakraProvider>
-  </React.StrictMode>
-  
-)
-
+import App from './App.jsx'
+import './index.css'
 import {
   Menu,
   MenuButton,
@@ -23,3 +13,14 @@ import {
   MenuOptionGroup,
   MenuDivider,
 } from '@chakra-ui/react'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider>
+    <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
+
+
