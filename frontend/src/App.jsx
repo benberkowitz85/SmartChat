69d8a3e17cf85a847c5a9b6a000437668c8eca0e
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2024-01-12 01:41:02 -0500

    Adding files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,15 @@
-import {Heading, Text, Box, Flex } from "@chakra-ui/react";
-import './App.css'
-import { Button, } from '@chakra-ui/react'
+import "./App.css";
+import Homepage from "./Pages/Homepage";
+import { Route } from "react-router-dom";
+import Chatpage from "./Pages/Chatpage";
 
 function App() {
   return (
-    <Flex
-        width={"100vw"}
-        heigth={"100vh"}
-        alignContent={"center"}
-        justifyContent={"center"}
-    >
-      <Box maxW="2xl" m=") auto">
-        <Heading as="h1" textAlign="center" fontSize="5xl" mt="100px">
-             Welcome to SmartChat!
-        </Heading>
-        <Text fontSize="xl" textAlign="center" mt="30px">
-        A Scholarly-focused chat, where researchers, academics, students and thinkers can find articles, create discussion threads and find resources. 
-        </Text>
-        <Text
-          w="fit-content"
-          p="4"
-          px="50"
-          bg="blue.300"
-          borderRadius="10px"
-          m="0 auto"
-          mt="8"
-          fontWeight="bold"
-          color="white"
-          fontSize="xl"
-      >
-        Get Started
-      </Text>
-      </Box>
-    </Flex>   
-      
-  )
+    <div className="App">
+      <Route path="/" component={Homepage} exact />
+      <Route path="/chats" component={Chatpage} />
+    </div>
+  );
 }
 
 export default App;
