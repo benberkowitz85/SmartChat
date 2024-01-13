5c98612b125df42a1a508660bd11255e37d26e96
Katherine-V <msvogler919@gmail.com>
2024-01-13 17:15:39 -0500

    Update index.js
    added "chakra provider" 2 lines

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Chakra Provider>
     <App />
+  </Chakra Provider>
   </React.StrictMode>
 );
