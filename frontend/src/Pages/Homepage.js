8a61d9701426c20b7951530f878216a49735435a
Katherine V <msvogler919@gmail.com>
2024-01-14 13:40:18 -0500

    adding code to homepage.js

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Box, Container } from "@chakra-ui/react";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+   <Container maxW = 'xl' centerContent>
+     <Box
+       d= 'flex'
+       justifyContent= 'center'
+       p={3}
+       bg={"white"}
+       w="100%"
+       m="40px 0 15px 0"
+       borderRadius="lg"
+       borderWidth="1px"
+     >
+       <Text fontSize="4xl" fontFamily="Work sans" color="black">
+         SmartChat
+       </Text>
+     </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px"></Box>
+
+   </Container>
+
+<div>Homepage</div>
   )
 }
 
