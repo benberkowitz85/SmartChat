7f4e1b9d1afb77ba08e0417412501143e16b8301
Michaelepigott <michaelepigott@gmail.com>
2024-01-14 14:39:59 -0500

    homepage tabs setup but empty

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import {Container, Box, Text, Tab, Tablist, TabPanel, TabPanels, Tabs,} from  "@chakra-ui/react";
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+      <Container maxW='xl' centerContent>
+        <Box
+        d='flex'
+        justifyContent="center"
+        p={3}
+        bg='white'
+        m='40px 0 15px 0'
+        borderRadius='lg'
+        borderWidth='1px'
+        >
+          <Text fontSize='4xl' color='black'>SmartChat</Text>
+        </Box>
+        <Box bg='white' w='100%' p={4} borderRadius='lg' borderWidth='1px' color ='black'>
+          <Tabs variant='soft-rounded'>
+            <Tablist mb='1em'>
+              <Tab width='50%'>Login</Tab>
+              <Tab width='50%'>Signup</Tab>
+            </Tablist>
+            <TabPanels>
+              <TabPanel>
+              {/*Login goes here*/}
+              </TabPanel>
+              <TabPanel>
+                {/*Login goes here*/}
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Container>
   )
 }
 
