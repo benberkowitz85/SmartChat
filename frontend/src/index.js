e525a6ae04e78f772f1892c8732abcc1a7bf8ace
lyubomirg1995 <lyubomirg17@gmail.com>
2024-01-14 20:01:29 -0500

    add: scrollable chat

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react' 
-import {BrowserRouter} from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react';
+import {BrowserRouter} from 'react-router-dom';
+import ChatProvider from "./Context/ChatProvider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider>
+ ReactDOM.render(
+<BrowserRouter>
+  <ChatProvider>
+    <ChakraProvider>
         <App />
       </ChakraProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  </ChatProvider>
+    </BrowserRouter>,
+ document.getElementById('root')
 );
 
