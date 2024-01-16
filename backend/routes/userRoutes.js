a1ee9102ad15a3948e63d3313196bb75cfe7990c
lyubomirg1995 <lyubomirg17@gmail.com>
2024-01-16 12:56:44 -0500

    first round of debugging

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,9 +3,9 @@ const { registerUser, authUser, allUsers} = require("../controllers/userControll
 const {protect} = require("../middleware/authMiddleware");
 const router = express.Router();
 
+
 router.route("/").post(registerUser);
- 
-router.route('/').post(registerUser).get(protect,allUsers);  
+router.route('/').get(protect, allUsers);  
 router.post("/login", authUser)
 
 
